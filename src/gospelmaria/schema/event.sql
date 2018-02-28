b7acfe0c238945d7c9546a84168c4abbdcb2967f
James Harris <james.harris@icecave.com.au>
2018-02-28 10:30:46 +1000

    Fix whitespace/indenting in SQL schema [ci skip]

--- src/gospelmaria/schema/event.sql
+++ src/gospelmaria/schema/event.sql
@@ -3,14 +3,14 @@
 --
 CREATE TABLE IF NOT EXISTS event
 (
-   id           BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-   store_id     BIGINT UNSIGNED NOT NULL,
-   event_type   VARBINARY(255) NOT NULL,
-   content_type VARBINARY(255) NOT NULL,
-   body         LONGBLOB,
-   time         TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    id           BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    store_id     BIGINT UNSIGNED NOT NULL,
+    event_type   VARBINARY(255) NOT NULL,
+    content_type VARBINARY(255) NOT NULL,
+    body         LONGBLOB,
+    time         TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
 
-   INDEX event_type (event_type),
-   INDEX archive (time, store_id)
+    INDEX event_type (event_type),
+    INDEX archive (time, store_id)
 )
 ROW_FORMAT=COMPRESSED;
